3d55113e2e5213c33ddbe3908681ea00ff72df24
jannunzi <jannunzi@gmail.com>
2018-06-14 21:44:55 -0400

    added form creator

--- models/forms/form.schema.server.js
+++ models/forms/form.schema.server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const formElementSchema = require('./form-element.schema.server');
 const formSchema = mongoose.Schema({
+  creator: String,
   name: String,
   elements: [formElementSchema]
 }, {collection: 'forms'});
